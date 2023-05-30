a5b44917ae98894ef1d0f77e68988f541fcff25c
shubhamparmekar <101289021+shubhamparmekar@users.noreply.github.com>
2023-05-30 17:05:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# FIFA-23-PLAYER-MANAGEMENT-SYSTEM
+ # FIFA-18-Management-System (FMS)
+
+This repository contains all the project files and necessary details about applications required to run the project on your local machine.
+
+***NOTE: Please read the installation and execution steps provided below before downloading. Thank you.***
+
+***Project demo: [Project demo video link](https://drive.google.com/open?id=1dcTCe_G5rUe6ibVh3Yd_pe3PIPPDiJ1c)***
+
+## About FMS
+
+FIFA 18 Player management system is a player management software for monitoring and accessing players based on their FIFA 18 PC/XBOX game ratings. HTML, CSS, and JavaScript were used for front-end development, and PHP and MySQL were used for back-end development. The system mainly focuses on basic operation like adding a new player, new statistics, searching players with detailed information and edit as they grow their skills. It is a web-based application designed and developed to help users access players and organize their teams. It’s easy to use, and it features a familiar and attractive user interface combined with strong searching, insertion, and deletion with procedure capabilities.
+
+## FMS requirements
+
+One of the most difficult tasks is the selection of correct version of software. Once the system requirements are known the next step is to determine whether the software package fits the requirements. After initial selection further security is needed to determine the desirability of software compared with other candidates. This section first summarizes the application requirement question and then suggests more detailed comparisons.
+
+### Hardware Requirement
+
+1.	32/64-bit processor
+2.	i3 or greater intel processor chip
+3.	1.7 or more GHz processor
+
+### Software Requirement
+1.	Windows 7 or higher version OS
+2.	Google chrome v70.0.3538 or greater
+3.	WAMPP web server
+4.	Brackets web editor
+
+## Description of FMS
+
+This project consists of player details which include the likes of player’s age and nationality. It also consists of player statistics which include players technical skills. It also consists of tables containing information such as player earnings, club, and preferred position to play. It also provides a strong search, update, delete, and insert operations delivered with a user-friendly web-based UI. The project also helps the users to keep track of the player details in a computerized way without any trouble. The project contains 7 stored procedures and 3 triggers per table. Stored procedures are used in search engine. Every time the user searches through the database, a procedure is called, and the results is collected and displayed for the user in a structured manner. It also has 3 triggers namely “Insert, Delete and Update” triggers assigned separately to each table. Whenever operations such as insert or delete or update is performed on any table, these triggers are automatically called, and the logs are captured into 3 separate tables, individually for each trigger. Hence use of triggers provides users to trace back all the latest as well as the oldest changes into any table at any point of time.
+
+## Installation and execution procedure
+
+1. Install wamp [Download wamp from here](https://sourceforge.net/projects/wampserver/files/latest/download) 299Mb and update google chrome [download latest chrome from here](https://www.google.com/chrome/).
+2. After installing wamp (Default directory : c:/wamp64/) , download the project and paste it in directory : (c:/wamp64/www/).
+3. Set your wamp **username to root** and no password. [Instructions to change username and password](https://hsnyc.co/how-to-set-the-mysql-root-password-in-localhost-using-wamp/)
+4. Start wampServer64 from the desktop icon and open google chrome and type the following url without quotes: "http://localhost/phpmyadmin/" and enter root as username and press Go.
+5. Now first you have to Load the database in your local server and then you can run the project. 
+     
+     To load the database :
+        
+        - Click on +New on the left hand column
+        - Give database name as "fifa" (without quotes and small case) and set character encoding to "utf8mb4_unicode_ci"
+        - After creating the database successfully, on the upper main menu panel, click on Import and then click "choose file" from file to import menu. Now browse to directory where you saved the project (expected directory: c://wamp/www/your_project_name/db/fifa.sql) and click on fifa.sql and then go down and click Go (Do not change any other settings).
+        - After importing successfully, loading the database is complete.
+      
+     Run the project :
+      
+          - Open a new tab in chrome
+          - type the following url : http://localhost/your_project_name_inside_www_directory/index.html
+          - enjoy.
+ 
+## Linting tool used:
+1. JSLint 
+
+For further queries : Drop me mail at pklappy21@gmail.com
---
+++ database/database.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Database</title>
+  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel='stylesheet' href='css/34b729901a37198f5d0414728.css'>
+      <link rel="stylesheet" href="css/style.css">
+      <link href='https://fonts.googleapis.com/css?family=Codystar' rel='stylesheet' type='text/css'>
+      <link href="css/menu.css" rel="stylesheet" type="text/css">
+        <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js" type="text/javascript"></script>   
+      <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
+  
+</head>
+
+<body style="background-image: linear-gradient(to right top, #1600ff, #9300a5, #930061, #751039, #4c2e2e);">
+    <canvas class="fireworks">
+        
+      </canvas>
+  <section>
+      <ul class="menu cf navbar">
+          <li><a href="../INDEX.html">Home</a></li>
+          <li><a href="../search_player/player_search.html">Search</a> </li>
+          <li><a href="../update_player/update_player.html">Update</a></li>
+          <li><a href="../insert_player/insert_new_player.html">Insert</a></li>
+          <li><a href="database.php">Database</a></li>
+            <li><a href="../report/project_report.html">Report</a></li>
+          <li><a href="../procedures/procedures.html">Procedures</a></li>
+          <li><a href="../about/about.html">About</a></li>
+        </ul>  <br>      
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fifa";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+} 
+?>
+  <h1>PERSONAL DETAILS</h1>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th>PLAYER ID</th>
+          <th>NAME</th>
+          <th>AGE</th>
+          <th>OVERALL RATING</th>
+          <th>NATIONALITY</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+<div class="tbl-content">
+<table cellpadding="0" cellspacing="0" border="0">
+<!--<tbody>-->
+<?php
+$sql = "SELECT * FROM personal_details ORDER BY id";
+$result = $conn->query($sql);
+if ($result->num_rows >0 ) {
+    while($row = $result->fetch_assoc()) { 
+        echo "<tbody>";
+        echo "<tr>";
+         echo "<td>" .$row["player_id"]. "</td>";
+          echo "<td>" .$row["player_name"]. "</td>";
+          echo "<td>" .$row["age"]. "</td>";
+         echo "<td>" .$row["overall_rating"]. "</td>";
+          echo "<td>" .$row["nationality"]. "</td>";
+         echo "</tr>";
+        echo "</tbody>";
+    }
+}else {
+     echo "0 results";
+ }    
+?>
+<!-- </tbody>-->
+</table>
+</div><br>
+
+  <h1>PLAYER POSITIONS</h1>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th>PLAYER ID</th>
+          <th>GOALKEEPER</th>
+          <th>DEFENDER</th>
+          <th>CENTRAL-MID</th>
+          <th>FORWARD</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+<div class="tbl-content">
+<table cellpadding="0" cellspacing="0" border="0">
+<!--<tbody>-->
+<?php
+$sql = "SELECT * FROM position ORDER BY id";
+$result = $conn->query($sql);
+if ($result->num_rows >0 ) {
+    while($row = $result->fetch_assoc()) { 
+        echo "<tbody>";
+        echo "<tr>";
+         echo "<td>" .$row["player_id"]. "</td>";
+          echo "<td>" .$row["gk"]. "</td>";
+          echo "<td>" .$row["df"]. "</td>";
+         echo "<td>" .$row["cm"]. "</td>";
+          echo "<td>" .$row["fr"]. "</td>";
+         echo "</tr>";
+        echo "</tbody>";
+    }
+}else {
+     echo "0 results";
+ }    
+?>
+<!-- </tbody>-->
+</table>
+</div><br>
+      
+  <h1>PLAYER EARNINGS</h1>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th>PLAYER ID</th>
+          <th>WAGE</th>
+          <th>VALUE</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+<div class="tbl-content">
+<table cellpadding="0" cellspacing="0" border="0">
+<!--<tbody>-->
+<?php
+$sql = "SELECT * FROM salary ORDER BY id";
+$result = $conn->query($sql);
+if ($result->num_rows >0 ) {
+    while($row = $result->fetch_assoc()) { 
+        echo "<tbody>";
+        echo "<tr>";
+         echo "<td>" .$row["player_id"]. "</td>";
+          echo "<td>" .$row["wage"]. "</td>";
+          echo "<td>" .$row["value"]. "</td>";
+         echo "</tr>";
+        echo "</tbody>";
+    }
+}else {
+     echo "0 results";
+ }    
+?>
+<!-- </tbody>-->
+</table>
+</div><br>
+        <h1>PLAYER STATS</h1>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th>PLAYER ID</th>
+        <th>ACCELERA TION</th>
+            <th>BALANCE</th>
+            <th>BALL CONTROL</th>
+            <th>CROSSING</th>
+            <th>CURVE</th>
+            <th>DRIBBLING</th>
+            <th>FINISHING</th>
+            <th>GK KICKING</th>
+            <th>GK POSITIONING</th>
+            <th>PENALTIES</th>
+            <th>SHORT PASS</th>
+            <th>STAMINA</th>
+            <th>STRENGTH</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+<div class="tbl-content">
+<table cellpadding="0" cellspacing="0" border="0">
+<!--<tbody>-->
+<?php
+$sql = "SELECT * FROM player_stats ORDER BY id";
+$result = $conn->query($sql);
+if ($result->num_rows >0 ) {
+    while($row = $result->fetch_assoc()) { 
+        echo "<tbody>";
+        echo "<tr>";
+         echo "<td>" .$row["player_id"]. "</td>";
+        echo "<td>" .$row["acceleration"]. "</td>";
+          echo "<td>" .$row["balance"]. "</td>";
+         echo "<td>" .$row["ball_control"]. "</td>";
+         echo "<td>" .$row["crossing"]. "</td>";
+         echo "<td>" .$row["curve"]. "</td>";
+         echo "<td>" .$row["dribbling"]. "</td>";
+         echo "<td>" .$row["finishing"]. "</td>";
+         echo "<td>" .$row["gk_kicking"]. "</td>";
+         echo "<td>" .$row["gk_positioning"]. "</td>";
+         echo "<td>" .$row["penalties"]. "</td>";
+         echo "<td>" .$row["short_pass"]. "</td>";
+         echo "<td>" .$row["stamina"]. "</td>";
+         echo "<td>" .$row["strength"]. "</td>";
+         echo "</tr>";
+        echo "</tbody>";
+    }
+}else {
+     echo "0 results";
+ }    
+?>
+<!-- </tbody>-->
+</table>
+</div><br>
+            
+  <h1>PLAYER CLUB</h1>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th>PLAYER ID</th>
+          <th>CLUB</th>
+          <th>PREFERRED POSITION</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+<div class="tbl-content">
+<table cellpadding="0" cellspacing="0" border="0">
+<!--<tbody>-->
+<?php
+$sql = "SELECT * FROM other_details ORDER BY id";
+$result = $conn->query($sql);
+if ($result->num_rows >0 ) {
+    while($row = $result->fetch_assoc()) { 
+        echo "<tbody>";
+        echo "<tr>";
+         echo "<td>" .$row["player_id"]. "</td>";
+          echo "<td>" .$row["club"]. "</td>";
+          echo "<td>" .$row["preferred_position"]. "</td>";
+         echo "</tr>";
+        echo "</tbody>";
+    }
+}else {
+     echo "0 results";
+ }  
+$conn-> close();
+?>
+<!-- </tbody>-->
+</table>
+</div><br>
+      
+  <defs>
+      <radialGradient cx="50%" cy="0%" fx="50%" fy="0%" r="50%" id="radialGradient-1">
+        <stop stop-color="#329FFF" offset="0%"></stop>
+        <stop stop-color="#206EFF" offset="100%"></stop>
+      </radialGradient>
+      <radialGradient cx="50%" cy="0%" fx="50%" fy="0%" r="50%" id="radialGradient-2">
+        <stop stop-color="#FF7894" offset="0%"></stop>
+        <stop stop-color="#FF324A" offset="100%"></stop>
+      </radialGradient>
+      <radialGradient cx="50%" cy="0%" fx="50%" fy="0%" r="100%" id="radialGradient-3">
+        <stop stop-color="#FF7894" offset="0%"></stop>
+        <stop stop-color="#FF324A" offset="100%"></stop>
+      </radialGradient>
+      <radialGradient cx="50%" cy="0%" fx="50%" fy="0%" r="100%" id="radialGradient-4">
+        <stop stop-color="#359FFC" offset="0%"></stop>
+        <stop stop-color="#206EFF" offset="100%"></stop>
+      </radialGradient>
+      <radialGradient cx="50%" cy="0%" fx="50%" fy="0%" r="50%" id="radialGradient-5">
+        <stop stop-color="#5FFFD2" offset="0%"></stop>
+        <stop stop-color="#31FFA6" offset="100%"></stop>
+      </radialGradient>
+    </defs>
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <rect id="dot-js" x="80" y="352" width="32" height="32" rx="16"></rect>
+    </g>
+</section>
+
+
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+
+    <script src='js/4032af61ca0478304ab7b31b7.js'></script>
+
+    <script  src="js/index.js"></script>
+
+
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
+
+</html>
